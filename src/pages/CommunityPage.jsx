3ae3e6e2978db67df3f506b73cf0a1e36516132f
Pranavi <pranaviparasu@gmail.com>
2025-06-26 16:11:52 +1000

    Added base layout for Community Page

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -3,8 +3,30 @@ import { Link } from 'react-router-dom';
 export const CommunityPage = () => {
   return (
     <PhoneFrame>
-      <h1 className="text-xl font-bold">Welcome</h1>
-      <p className="text-gray-600">This is the community page of the app.</p>
+      {/* <h1 className="text-xl font-bold">Welcome</h1>
+      <p className="text-gray-600">This is the community page of the app.</p> */}
+
+      {/* HEADER */}
+
+      {/* Banner */}
+      <div>
+        <h1>SharePlate Community</h1>
+
+        {/* Image */}
+
+        {/* Seperator */}
+
+        {/* Tabs */}
+
+      </div>
+
+      {/* Search Bar */}
+
+      {/* Drop down for filtering */}
+
+      {/* Cards for listings */}
+
+      {/* FOOTER */}
     </PhoneFrame>
   );
 };
