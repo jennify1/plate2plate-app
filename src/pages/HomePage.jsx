fc28747f288ac6a7df6db84e21009c88d8911d8e
Iqtidar Rahman <z5361409@ad.unsw.edu.au>
2025-07-03 21:06:07 +1000

    Just added the main 3 option texts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,27 @@
 import PhoneFrame from "../components/PhoneFrame";
 import { Link } from 'react-router-dom';
+import Card from '../components/notifications/Card';
+
 export const HomePage = () => {
   return (
     <PhoneFrame>
       <h1 className="text-xl font-bold">Welcome</h1>
+      
       <p className="text-gray-600">This is the home page of the app.</p>
+      <Card>
+        sdfsdsdf
+      </Card>
+
+      {/* Make into component similar to "Card" later  */}
+      <div>
+
+      </div>
+      
+      <p className="text-gray-600">To share!</p>
+
+      <p className="text-gray-600">Sharing~</p>
+      <p className="text-gray-600">Community</p>
+
       <Link
         to="/login"
         className="inline-block bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
