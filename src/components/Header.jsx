df7bc71719921c134fa114e6dd0a542e0d2d23c7
Jennifer Yu <z5480946@ad.unsw.edu.au>
2025-07-05 21:16:35 +1000

    Fix: spacing from top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import HeaderIcon from '../assets/HeaderIcon.png'
 
 export const Header = () => (
-  <header className="relative flex items-center h-10">
+  <header className="relative flex items-center h-10 mt-3">
     <img
       src={HeaderIcon}
-      alt="SharePlate logo"
+      alt="plate2plate logo"
       className="absolute left-4 w-7 h-7"
     />
     <h1 className="mx-auto text-xl font-semibold text-[#AAD399]">
-      SharePlate
+      plate2plate
     </h1>
   </header>
 )
