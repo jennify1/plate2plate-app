5deb1441c75ee66a74a5ec1c948fc1bdbc93660e
Jennifer Yu <z5480946@ad.unsw.edu.au>
2025-07-05 21:16:43 +1000

    Add: header to phoneframe component

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -1,14 +1,10 @@
 // components/PhoneFrame.jsx
-
+import { Header } from "./Header";
 const PhoneFrame = ({ children }) => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="w-[375px] h-[667px] bg-white rounded-2xl shadow-xl overflow-hidden border border-gray-300">
-        {/* Simulate top bar */}
-        <div className="h-10 bg-gray-200 flex items-center justify-center text-sm font-semibold">
-          Shareplate
-        </div>
-
+        <Header> plate2plate </Header>
         {/* Main content */}
         <div className="h-[calc(100%-2.5rem)] overflow-y-auto p-4">
           {children}
